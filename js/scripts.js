f990ef2e2ed08edf2b815508c523cdad5cf3d218
Arianna Chau and Emily Dokken <arianna@>
2017-02-01 20:08:00 -0800

    Add function to add, subtract, multiply and divide in JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,44 @@
-var add = function(number1, number2) {
-  return number1 + number2;
+//Addition Function
+
+var add = function(sum1, sum2) {
+  return sum1 + sum2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+var number1 = parseInt(prompt("Enter a number to add:"));
+var number2 = parseInt(prompt("Enter another number to add:"));
 
 alert(add(number1, number2));
+
+//Subtraction Function
+
+var subtract = function(difference1, difference2) {
+  return difference1 - difference2;
+};
+
+var number3 = parseInt(prompt("Enter a number to subtract:"));
+var number4 = parseInt(prompt("Enter another number to subtract:"));
+
+alert(subtract(number3, number4));
+
+// //Multiplication Function
+
+var multiply = function(product1, product2) {
+  return product1 * product2;
+};
+
+ var number5 = parseInt(prompt("Enter a number to multiply:"));
+ var number6 = parseInt(prompt("Enter another number to multiply:"));
+
+
+alert(multiply(number5, number6));
+
+// //Division Function
+
+var divide = function(quotient1, quotient2) {
+  return quotient1 / quotient2;
+};
+
+var number7 = parseInt(prompt("Enter a number to divide:"));
+var number8 = parseInt(prompt("Enter another number to divide:"));
+
+alert(divide(number7, number8));
